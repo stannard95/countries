ec5e583c0571d6aba52408e73a3a8da283b4cd82
stannard95 <14066588@brookes.ac.uk>
2017-10-03 09:54:22 +0100

    finished

--- app/controllers/countries_controller.rb
+++ app/controllers/countries_controller.rb
@@ -4,20 +4,46 @@ class CountriesController < ApplicationController
   end
 
   def show
+    @country = Country.find(params[:id])
   end
 
-  def edit
+    def edit
+    @country = Country.find(params[:id])
   end
 
   def create
+    country = Country.new
+    country.name = params[:name]
+    country.pop = params[:pop]
+    country.flag_url = params[:flag_url]
+    country.language = params[:language]
+    country.president = params[:president]
+    country.save
+
+    redirect_to "/countries"
   end
 
   def new
+
   end
 
   def update
+    id = params[:id]
+    country = Country.find(id)
+    country.name = params[:name]
+    country.pop = params[:pop]
+    country.flag_url = params[:flag_url]
+    country.language = params[:language]
+    country.president = params[:president]
+    country.save
+
+    redirect_to "/countries"
   end
 
   def destroy
+    id = params[:id]
+    Country.destroy(id)
+    redirect_to "/countries"
+
   end
 end
